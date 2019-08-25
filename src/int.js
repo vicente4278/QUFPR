b37cdc1404d70f2452405a403314b1a225e79dfd
Vicente Kalinoski Parmigiani <vicenteparmigiani@gmail.com>
2019-08-25 20:21:23 -0300

    Scroll down fix

--- src/int.js
+++ src/int.js
@@ -7,7 +7,7 @@ function scrollToTop() {
 	else clearTimeout(timeOut);
 }
 
-var scroll = 0;
+/** var scroll = 0;
 
 function Down() {
 	document.getElementById('pre-body').className = "Scrolled";
@@ -18,12 +18,16 @@ function Up() {
 	document.getElementById('pre-body').className = "";
 	document.getElementById('mainT').className = "pre-body-title";
 }
-
+**/
 function goUpB() {
 	if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0){
 		document.getElementById("go2Top").className = "TopH";
+		document.getElementById('pre-body').className = "";
+		document.getElementById('mainT').className = "pre-body-title";
 	}
 	if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0){
 		document.getElementById("go2Top").className = " ";
+		document.getElementById('pre-body').className = "Scrolled";
+		document.getElementById('mainT').className = "pre-body-titleScroll";
 	}
 }
