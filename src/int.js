55cf56a865b54cbd641779234b780c18aeb79c1c
Vicente Kalinoski Parmigiani <vicenteparmigiani@gmail.com>
2019-08-25 19:55:57 -0300

    Fix Scroll

--- src/int.js
+++ src/int.js
@@ -1,14 +1,3 @@
-
-function SANDmenu(Smenu) {
-	if (Smenu == 0) {
-		document.getElementById("sandwish_menu").className = " ";
-		document.getElementById("darkBackground").className = "show";
-	} else {
-		document.getElementById("sandwish_menu").className = "SANDClose";
-		document.getElementById("darkBackground").className = "hide";
-	}
-}
-
 var timeOut;
 function scrollToTop() {
 	if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
@@ -23,12 +12,6 @@ var scroll = 0;
 function Down() {
 	document.getElementById('pre-body').className = "Scrolled";
 	document.getElementById('mainT').className = "pre-body-titleScroll";
-	if (scroll < 70){
-		window.scrollBy(0,10);
-		timeOut1=setTimeout('Down()',100);
-		scroll++;
-	}
-	else clearTimeout(timeOut1);
 }
 
 function Up() {
