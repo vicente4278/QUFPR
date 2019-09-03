88f0799c3847c327e56da22c621feca93fa8d85d
Vicente Kalinoski Parmigiani <vicenteparmigiani@gmail.com>
2019-09-02 21:10:58 -0300

    Home Page fix and link to home on every page

--- src/int.js
+++ src/int.js
@@ -24,19 +24,13 @@ var mode = 0;
 function goUpB() {
 	if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0){
 		document.getElementById("go2Top").className = "TopH";
-		document.getElementById('pre-body').className = "";
-		document.getElementById('mainT').className = "pre-body-title";
-		document.getElementById('subT').className = "pre-body-subtitle";
 	}
 	if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0){
 		document.getElementById("go2Top").className = " ";
-		document.getElementById('pre-body').className = "Scrolled";
-		document.getElementById('mainT').className = "pre-body-titleScroll";
-		document.getElementById('subT').className = "pre-body-s-titleScroll";
 
 	}
 }
 
 function load() {
-	setInterval('goUpB();',750)
+	setInterval('goUpB();',500)
 }
